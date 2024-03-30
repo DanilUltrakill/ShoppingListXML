bab5c862f17a702dd85b7a574285380e302150bf
DanilUltrakill <yaremakadanil@gmail.com>
2024-03-31 01:43:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/shoppinglistxml/domain/GetShopItemByIdUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shoppinglistxml.domain
+
+class GetShopItemByIdUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun getShopItem(shopItemId: Int): ShopItem {
+        return shopListRepository.getShopItem(shopItemId)
+    }
+}
